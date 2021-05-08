c3377e8f23b7b02d8808be24dd9198d0f7823157
David Hilton <david.hilton.p@gmail.com>
2021-05-08 19:51:36 -0400

    create file for writing, look at directories, write images found

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 find-images
 ===========
 
+Designed to produce a list of images for [jpegview](https://sourceforge.net/projects/jpegview/).
+
 The find-images binary takes as its arguments any number of directories.
 
 It writes to the parent directory in common a list of images it finds.
 
-Default image types:
-JPEG, BMP, PNG, WEBP, TGA, GIF and TIFF
+Image extensions:
+.jpg, .jpeg, .bmp, .png, .webp, .gif and .tiff, plus a bunch of raw formats.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,108 @@
+use std::ffi::OsStr;
+use std::fs;
+use std::path::{Path, PathBuf};
+
+use std::fs::{canonicalize, File};
+use std::io::{LineWriter, Write};
+use std::process::exit;
+use structopt::StructOpt;
+
+/// Returns true if `file` is a .jpg, .jpeg, .png, .bmp,
+///  .webp, .gif or .tiff,... plus a bunch of raw file
+///  extensions, handling case sensitivity.
+fn is_image(file: &Path) -> bool {
+    let image_types = [
+        "jpg", "jpeg", "png", "bmp", "webp", "gif", "tiff", "pef", "dng", "crw", "nef", "cr2",
+        "mrw", "rw2", "orf", "x3f", "arw", "kdc", "nrw", "dcr", "sr2", "raf",
+    ];
+
+    if let Some(extension) = file.extension() {
+        let extension = extension.to_ascii_lowercase();
+        for t in image_types.iter() {
+            if extension == OsStr::new(t) {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+#[test]
+fn test_is_image() {
+    assert_eq!(is_image(&PathBuf::from("foo")), false);
+    assert_eq!(is_image(&PathBuf::from("jpg")), false);
+    assert_eq!(is_image(&PathBuf::from("blah.jpg")), true);
+    assert_eq!(is_image(&PathBuf::from("blah.JPG")), true);
+}
+
+fn images(path: &Path) -> Vec<PathBuf> {
+    let mut images = Vec::new();
+    if let Ok(entries) = fs::read_dir(&path) {
+        for entry in entries.flatten() {
+            if let Ok(file_type) = entry.file_type() {
+                if file_type.is_file() && is_image(&entry.path()) {
+                    images.push(entry.path());
+                }
+            }
+        }
+    }
+    images
+}
+
+fn subdirs(path: &Path) -> Vec<PathBuf> {
+    let mut subdirs = Vec::new();
+    if let Ok(entries) = fs::read_dir(&path) {
+        for entry in entries.flatten() {
+            if let Ok(file_type) = entry.file_type() {
+                if file_type.is_dir() {
+                    subdirs.push(entry.path());
+                }
+            }
+        }
+    }
+    subdirs
+}
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(short, long, default_value = "images.txt")]
+    output: String,
+
+    #[structopt(parse(from_os_str))]
+    dirs: Vec<PathBuf>,
+    // #[structopt(default_value = "32", long)]
+    // images_per_dir: usize,
+    // todo: add recursive mode
+}
+
+fn process_dir(dir: &Path, file: &mut LineWriter<File>) {
+    println!("finding images in {:?}", &dir);
+    for image in images(&dir) {
+        file.write_all(&image.as_os_str().to_string_lossy().as_bytes())
+            .expect("unable to write file, aborting!");
+        file.write_all(b"\r\n")
+            .expect("unable to write file, aborting!");
+    }
+    for subdir in subdirs(&dir) {
+        process_dir(&subdir, file);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("{:?}", &args.output);
+    if let Ok(file) = File::create(&args.output) {
+        let mut file = LineWriter::new(file);
+        for dir in args.dirs {
+            if let Ok(dir) = canonicalize(&dir) {
+                process_dir(&dir, &mut file);
+            } else {
+                println!("{:?} does not exist?", &dir)
+            }
+        }
+    } else {
+        println!("unable to create {}", args.output);
+        exit(1);
+    }
 }
