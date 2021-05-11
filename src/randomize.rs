d71fa0ae71491d90378c9108f1b3092722b5cb00
David Hilton <david.hilton.p@gmail.com>
2021-05-11 17:18:54 -0400

    added randomize executable

---
+++ src/randomize.rs
@@ -0,0 +1,62 @@
+use std::env::current_dir;
+use std::fs::File;
+use std::io::{BufRead, BufReader, LineWriter, Write};
+use std::process::exit;
+
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(short, long, default_value = "images.txt")]
+    output: String,
+
+    #[structopt(short, long, default_value = "images.txt")]
+    input: String,
+}
+
+fn slurp(input: &str) -> Vec<String> {
+    if let Ok(file) = File::open(&input) {
+        let mut retval = Vec::new();
+
+        let buf = BufReader::new(file);
+        for line in buf.lines().flatten() {
+            println!("{:?}", &line);
+            retval.push(line);
+        }
+        retval
+    } else {
+        if let Ok(current_dir) = current_dir() {
+            eprintln!("unable to read {:?} in {:?}", &input, current_dir);
+        }
+        exit(1);
+    }
+}
+
+fn write(output: &str, lines: Vec<String>) {
+    if let Ok(file) = File::create(&output) {
+        if let Ok(current_dir) = current_dir() {
+            eprintln!("writing output to {:?} in {:?}", &output, current_dir);
+        }
+        let mut file = LineWriter::new(file);
+        for line in lines {
+            file.write_all(line.as_bytes())
+                .expect("unable to write file!");
+            file.write_all(b"\n").expect("unable to write file!");
+        }
+    } else {
+        if let Ok(current_dir) = current_dir() {
+            eprintln!("unable to create {:?} in {:?}", &output, current_dir);
+        }
+        exit(2);
+    }
+}
+
+fn main() {
+    let args = Cli::from_args();
+
+    let mut lines = slurp(&args.input);
+    lines.shuffle(&mut thread_rng());
+    write(&args.output, lines);
+}
